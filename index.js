2d9b4150fcfe3805c6d225ae1d06775b235641f3
GOKUL.A.N <113367457+GOKUL-A-N@users.noreply.github.com>
2024-07-16 16:35:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+//problem 1 -> array creation and initialization
+//creation of array
+let cars = ["BMW","BENZ","Rolls Royce"];
+// initialization of array
+let numbers =[]
+
+// problem 2 -> array manipulation
+// push element to array 
+numbers.push(2);
+numbers.push(3);
+numbers.push(4);
+numbers.push(5);
+numbers.push(6);
+numbers.push(3);
+// shift element
+numbers.shift(2);
+// unshift element
+numbers.unshift(2);
+// pop element
+numbers.pop();
+
+
+// problem 3 -> array searching
+// index of a particular element
+console.log(numbers.indexOf(2))
+// finding last occuring index of a particular element
+console.log(numbers.lastIndexOf(2))
+//checking whether a particular element is present in the array
+console.log(numbers.includes(2))
+// searching elements using index
+console.log(numbers[3]);
+
+// Problem 4 ->array filtering
+// filtering array elements to another array 
+const num = numbers.filter((x) => x % 2 === 0);
+console.log(num)
+
+// Problem 5 -> array mapping
+const mapNum = numbers.map((x) => x*2)
+console.log(mapNum)
+
+// Problem 6 -> Sorting and reversing
+// sorting
+console.log(numbers.sort())
+// reverse an array
+console.log(numbers.reverse())
+
+// Problem 7 -> finding minimun and maximum
+let min = numbers[0];
+let max = numbers[0];
+for(let i = 1; i < numbers.length; i++){
+    if(min > numbers[i]) min = numbers[i];
+    if(max < numbers[i]) max = numbers[i];
+}
+console.log(min + " " + max); 
+
+
+// Problem 8 -> deduplicating the array
+const numWithoutDuplicates = []
+numbers.forEach((x) => {
+    if(!numWithoutDuplicates.includes(x)) numWithoutDuplicates.push(x);
+})
+
+console.log(numWithoutDuplicates)
+
+//Problem 9 -> intersection of two arrays
+let arr1=[1,2,3,4,5,6,7,8]
+let arr2=[1,2,4,5,6,]
+let res = []
+arr1.forEach(element => {
+    if(arr2.includes(element)) res.push(element)
+});
+
+console.log(res)
+
+// Problem 10 -> Implementing stack with array
+let stack = []
+function isEmpty(){
+    return stack.length === 0;
+}
+function push(element){
+    stack.push(element);
+    return val;
+}
+function pop(){
+    let val = stack[stack.length - 1];
+    stack.pop();
+    return val;
+}
+function peek(){
+    return stack[stack.length - 1];
+}
+
